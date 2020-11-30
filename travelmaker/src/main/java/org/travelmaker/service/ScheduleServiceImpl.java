263f324cb2dc6b6b8170c441debb87baa1403d61
Jinnie-J <jjesweet@naver.com>
2020-11-30 17:31:55 +0900

    게시물 상세작성 다중업로드 기능 구현

--- travelmaker/src/main/java/org/travelmaker/service/ScheduleServiceImpl.java
+++ travelmaker/src/main/java/org/travelmaker/service/ScheduleServiceImpl.java
@@ -37,18 +37,19 @@ public class ScheduleServiceImpl implements ScheduleService{
 	
 	@Override
 	public void statusupdate(int schNo) {
-		ScheduleVO schedule= mapper.read(schNo);
+		ScheduleVO schedule= get(schNo);
 		String status=schedule.getSchStatus();
+		System.out.println(status);
 		//작성일때 -> 미작성으로
 		if(status.equals("작성")) {
 			mapper.statusunWritten(schNo);
 		}
 		//작성중일때-> 작성으로
-		if(status.equals("작성중")) {
+		else if(status.equals("작성중")) {
 			mapper.statusWritten(schNo);
 		}
 		//미작성일때 -> 작성중으로
-		if(status.equals("미작성")) {
+		else if(status.equals("미작성")) {
 			mapper.statusWritting(schNo);
 		}
 	
