87ee4bc1b1bef7e1ad4878d2d6775cc8820b701c
fre_edom@naver.com <fre_edom@naver.com>
2021-01-04 14:56:27 +0900

    차트 색상 좀 더 보기좋게 변경함, 게시글 등록일 문제있는 것 수정함

--- travelmaker/src/main/java/org/travelmaker/controller/AdminMainController.java
+++ travelmaker/src/main/java/org/travelmaker/controller/AdminMainController.java
@@ -1,8 +1,5 @@
 package org.travelmaker.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -13,11 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.travelmaker.domain.BoardVO;
-import org.travelmaker.domain.QnABoardVO;
 import org.travelmaker.domain.StatisticVO;
 import org.travelmaker.service.MainService;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -27,32 +21,30 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/admin/*")
 @AllArgsConstructor
 public class AdminMainController {
-	
+
 	private MainService service;
-	
+
 	@GetMapping("/main")
-	public String showChart(Model model){
-		
+	public String showChart(Model model) {
+
 		List<StatisticVO> list = service.getChart("daily");
-		
+
 		model.addAttribute("today", list.get(0));
-		
-		model.addAttribute("qna",service.getQnaInfo());
-		
-		
+
+		model.addAttribute("qna", service.getQnaInfo());
+
 		return "main";
 	}
-	
-	
+
 	@GetMapping("/getChart/{type}")
-	public ResponseEntity<List<StatisticVO>> getChart(@PathVariable("type") String type){
-		
+	public ResponseEntity<List<StatisticVO>> getChart(@PathVariable("type") String type) {
+
 		return new ResponseEntity<>(service.getChart(type), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/getPostByPopularity/{type}")
-	public ResponseEntity<List<BoardVO>> getPostByPopularity(@PathVariable("type") String type){
-		
+	public ResponseEntity<List<BoardVO>> getPostByPopularity(@PathVariable("type") String type) {
+
 		return new ResponseEntity<>(service.getPostByPopularity(type), HttpStatus.OK);
 	}
 
